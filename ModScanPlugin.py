a5bef8f39f316d0cd9e6b3a01e4e83bf25ed4900
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-09-24 11:13:21 +0200

    Log classification text

--- ModScanPlugin.py
+++ ModScanPlugin.py
@@ -5,5 +5,5 @@ logging.basicConfig(filename='~/modscanplugin.log',level=logging.DEBUG)
 
 class ModScanPlugin(Plugin):
     def run(self, idmef):
-        logging.debug(str(idmef))
+        logging.debug(str(idmef.get("alert.classification.text")))
         logging.debug('\n\n\n')
