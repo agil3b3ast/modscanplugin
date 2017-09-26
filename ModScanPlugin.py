a33f3b3f5d22080aa5513333c9cf5671d2f35604
agil3b3ast <agil3b3ast@hotmail.it>
2017-09-26 13:58:27 +0200

    Added port scan correlation for a single source

--- ModScanPlugin.py
+++ ModScanPlugin.py
@@ -1,11 +1,24 @@
 from preludecorrelator.pluginmanager import Plugin
 import logging
 
+CLASSIFICATION = 'PortScan'
 print("### [ModScanPlugin] Log initialization ###")
 logging.basicConfig(filename='~/modscanplugin.log',level=logging.DEBUG)
-
+	
 class ModScanPlugin(Plugin):
     def run(self, idmef):
-        print idmef
+        #print idmef
         logging.debug(str(idmef.get("alert.classification.text")))
         logging.debug('\n\n\n')
+        source = IDMEF.get("alert.source(*).node.address(*).address")
+		
+		classification = IDMEF.get("alert.classification.text")
+		for saddr in source:
+		 if classification == CLASSIFICATION:
+          ctx = Context("PORT_SCAN_STORM", { "expire": 120, "threshold": 150, "alert_on_expire": True }, update = True, idmef = idmef)
+		  if ctx.getUpdateCount() == 0:
+		   ctx.set("alert.correlation_alert.name", "Port Scan Storm Detected")
+		   ctx.set("alert.classification.text", "PortScanStorm")
+		   ctx.set("alert.assessment.impact.severity", "high")
+    
+    	
