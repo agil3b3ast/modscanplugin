714868584773fffb2ee91db857d97689de1b5bfb
agil3b3ast <agil3b3ast@hotmail.it>
2017-09-26 17:25:02 +0200

    Changed get fields to support IDMEF-SCI

--- ModScanPlugin.py
+++ ModScanPlugin.py
@@ -1,4 +1,5 @@
 from preludecorrelator.pluginmanager import Plugin
+from preludecorrelator.idmef import IDMEF
 import logging
 
 ID_PORT_SCAN_DETECTED = "D1"
@@ -6,6 +7,8 @@ ID_PORT_SCAN_MONITORING = "D0"
 
 PORT_SCAN_DETECTED = "Port Scan Detected"
 PORT_SCAN_MONITORING = "Port Scan Monitoring"
+
+EVENT_ID = "event.id"
 
 print("### [ModScanPlugin] Log initialization ###")
 logging.basicConfig(filename='~/modscanplugin.log',level=logging.DEBUG)
@@ -15,8 +18,8 @@ class ModScanPlugin(Plugin):
     
 	def _PortScan(self, idmef):
         #print idmef
-        logging.debug(str(idmef.get("alert.classification.text")))
-        logging.debug('\n\n\n')
+        #logging.debug(str(idmef.get("alert.classification.text")))
+        #logging.debug('\n\n\n')
         source = IDMEF.get("alert.source(*).node.address(*).address")
 		
 		for saddr in source:
@@ -25,9 +28,17 @@ class ModScanPlugin(Plugin):
 				ctx.set("alert.correlation_alert.name", "Port Scan Storm Detected")
 				ctx.set("alert.classification.text", "PortScanStorm")
 				ctx.set("alert.assessment.impact.severity", "high")		
-		
+	
+    def _getDataByMeaning(self,meaning):
+	    meanings = IDMEF.get("alert.additional_data(*).meaning")
+		m_len = len(meanings)
+		for m in xrange(m_len):
+			if meanings[m] == meaning:
+			    to_search = "alert.additional_data({}).data".format(m)
+				d = IDMEF.get(to_search)
+                return d
+		return None	
+
     def run(self, idmef):
-		classification = IDMEF.get("alert.classification.text")
-		if classification == PORT_SCAN_DETECTED:
+		if  _getDataByMeaning(EVENT_ID) == ID_PORT_SCAN_DETECTED:
 			self._PortScan(idmef)			
-    	
