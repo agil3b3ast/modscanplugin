9a2a636d985587cb6048f1b4e059ad7772199914
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-09-27 12:25:06 +0200

    fixed idmef field bug

--- ModScanPlugin.py
+++ ModScanPlugin.py
@@ -27,8 +27,8 @@ class ModScanPlugin(Plugin):
 		#logging.debug(str(idmef.get("alert.classification.text")))
 		#logging.debug('\n\n\n')
 		#source = IDMEF.get("alert.source(*).node.address(*).address")
-		source = self._getDataByMeaning(IP_SRC)
-		dest = self._getDataByMeaning(IP_DST)
+		source = self._getDataByMeaning(idmef,IP_SRC)
+		dest = self._getDataByMeaning(idmef,IP_DST)
 		
 		ctx = Context(("PORT_SCAN_STORM",source,dest), { "expire": EXPIRATION, "threshold": THRESHOLD, "alert_on_expire": True }, update = True, idmef = idmef)
 		if ctx.getUpdateCount() == 0:
@@ -36,18 +36,18 @@ class ModScanPlugin(Plugin):
 			ctx.set("alert.classification.text", "PortScanStorm")
 			ctx.set("alert.assessment.impact.severity", "high")		
 
-	def _getDataByMeaning(self,meaning):
-		meanings = IDMEF.get("alert.additional_data(*).meaning")
+	def _getDataByMeaning(self,idmef,meaning):
+		meanings = idmef.get("alert.additional_data(*).meaning")
 		m_len = len(meanings)
 		for m in xrange(m_len):
 			if meanings[m] == meaning:
 				to_search = "alert.additional_data({}).data".format(m)
-				d = IDMEF.get(to_search)
+				d = idmef.get(to_search)
 				return d
 		return None	
 
 	def run(self, idmef):
-		ev_id = self._getDataByMeaning(EVENT_ID)		
+		ev_id = self._getDataByMeaning(idmef,EVENT_ID)		
 		if ev_id is None:
 			return		
 		if  ev_id == ID_PORT_SCAN_DETECTED:
